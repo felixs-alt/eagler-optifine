8be88fd75d7c6daa3013dcd3c9f8d74114023ace
PeytonPlayz595 <106421860+PeytonPlayz595@users.noreply.github.com>
2023-10-30 16:52:41 +0000

    Update main menu text

--- src/main/java/net/lax1dude/eaglercraft/v1_8/EaglercraftVersion.java
+++ src/main/java/net/lax1dude/eaglercraft/v1_8/EaglercraftVersion.java
@@ -7,9 +7,9 @@ public class EaglercraftVersion {
 	
 	/// Customize these to fit your fork:
 	
-	public static final String projectForkName = "EaglercraftX";
-	public static final String projectForkVersion = "u18";
-	public static final String projectForkVendor = "lax1dude";
+	public static final String projectForkName = "";
+	public static final String projectForkVersion = "";
+	public static final String projectForkVendor = "";
 	
 	public static final String projectForkURL = "https://gitlab.com/lax1dude/eaglercraftx-1.8";
 	
@@ -20,27 +20,27 @@ public class EaglercraftVersion {
 	// Do not change these, they must stay as credit to lax1dude's
 	// original repository for maintaining the project:
 
-	public static final String projectOriginName = "EaglercraftX";
-	public static final String projectOriginAuthor = "lax1dude";
-	public static final String projectOriginRevision = "1.8";
-	public static final String projectOriginVersion = "u18";
+	public static final String projectOriginName = "Shadow Client 3.0";
+	public static final String projectOriginAuthor = "PeytonPlayz585";
+	public static final String projectOriginRevision = "";
+	public static final String projectOriginVersion = "";
 	
-	public static final String projectOriginURL = "https://gitlab.com/lax1dude/eaglercraftx-1.8";
+	public static final String projectOriginURL = "https://github.com/PeytonPlayz595/Shadow-3.0";
 	
 	
 	// Miscellaneous variables:
 
-	public static final String mainMenuStringA = "Minecraft 1.8.8";
-	public static final String mainMenuStringB = projectOriginName + " " +
-			projectOriginRevision + "-" + projectOriginVersion + " ultimate";
+	public static final String mainMenuStringA = "";
+	public static final String mainMenuStringB = projectOriginName + "" +
+			projectOriginRevision + "" + projectOriginVersion + "";
 	public static final String mainMenuStringC = "";
-	public static final String mainMenuStringD = "Resources Copyright Mojang AB";
+	public static final String mainMenuStringD = "EaglercraftX 1.8.8";
 
-	public static final String mainMenuStringE = projectForkName + " " + projectForkVersion;
-	public static final String mainMenuStringF = "Made by " + projectForkVendor;
+	public static final String mainMenuStringE = projectForkName + "" + projectForkVersion;
+	public static final String mainMenuStringF = "";
 
-	public static final String mainMenuStringG = "Collector's Edition";
-	public static final String mainMenuStringH = "PBR Shaders";
+	public static final String mainMenuStringG = "Shadow Client 3.0";
+	public static final String mainMenuStringH = "Optifine + Shaders";
 
 	public static final boolean mainMenuEnableGithubButton = false;
 
